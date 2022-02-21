2cc04612761b0e8896d3b5deb7c413dfc75702fb
peacemaker4 <esmagametov.bek@gmail.com>
2022-02-22 03:09:21 +0600

    final+

--- js/index.js
+++ js/index.js
@@ -1,16 +1,51 @@
-function User(email, username, password){
+function User(email, username, password, description){
     this.email = email;
     this.username = username;
     this.password = password;
+    this.description = description;
 }
 
-function registerUser(user){
+function updateUser(user){
     localStorage.setItem("user", JSON.stringify(user));
 }
 
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show-filter");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show-filter");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 $(document).ready(function(){
 $("#register-form").submit(function () {
-    var emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     var email = $("#register-form").find('input[name="email-input"]').val();
     var username = $("#register-form").find('input[name="username-input"]').val();
@@ -24,10 +59,30 @@ $("#register-form").submit(function () {
         alert("Email format is wrong!");
     }
     else{
-        var user = new User(email, username, password);
-        registerUser(user);
+        var description = "Hello world!"
+        var user = new User(email, username, password, description);
+        updateUser(user);
     }
 })
+$("#update-form").submit(function () {
+    var email = $("#update-form").find('input[name="change-email-input"]').val();
+    var username = $("#update-form").find('input[name="change-username-input"]').val();
+    var description = $("#update-form").find('textarea[name="change-description-input"]').val();
+
+    if(!email.match(emailformat)){
+        alert("Email format is wrong!");
+    }
+    else{
+        var user = JSON.parse(localStorage.getItem("user"));
+        user.email = email;
+        user.username = username;
+        user.description = description;
+
+        updateUser(user);
+    }
+})
+
+
 $("#sound-btn-1").click(function(){
     var audio = new Audio('audio/good-ring.mp3');
     audio.play();
@@ -40,5 +95,21 @@ $("#sound-btn-3").click(function(){
     var audio = new Audio('audio/yeahbaby-ring.mp3');
     audio.play();
 })
+$("#sound-btn-4").click(function(){
+    var audio = new Audio('audio/rushe-ring.mp3');
+    audio.play();
+})
+$("#sound-btn-1").mouseover(function(){
+    $("#preview-image-1").attr("src", "img/preview-1.gif");
+})
+$("#sound-btn-1").mouseout(function(){
+    $("#preview-image-1").attr("src", "img/screenshot_3.png");
+})
+$("#search-input").keypress(function(){
+    var audio = new Audio('audio/key2-sound.mp3');
+    audio.play();
+})
+
+
 
 });
